Trott/async

commit cdfb4917e6028c8f966276d6e792018c7fd2ae3c

Fix an inefficient regex in autoInject (#1767)
* Fix an inefficient regex in autoInject

* 'properly strip comments in argument definitions' test failure

* Update test/autoInject.js

Co-authored-by: Rich Trott <rtrott@gmail.com>

* Update on url-comments lib/autoInject.js

Co-authored-by: Rich Trott <rtrott@gmail.com>

* move new tests test/autoInject.js

* indentation fix test/autoInject.js

Co-authored-by: Rich Trott <rtrott@gmail.com>

lib/autoInject.js
@@ -6,7 +6,7 @@ var FN_ARGS = /^(?:async\s+)?(?:function)?\s*\w*\s*\(\s*([^)]+)\s*\)(?:\s*{)/;
 var ARROW_FN_ARGS = /^(?:async\s+)?\(?\s*([^)=]+)\s*\)?(?:\s*=>)/;
 var FN_ARG_SPLIT = /,/;
 var FN_ARG = /(=.+)?(\s*)$/;
-var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+var STRIP_COMMENTS = /(\/\*(?:[^/]|\/(?!\*))*\*\/)|\/\/.*$/mg;
 
 function parseParams(func) {
     const src = func.toString().replace(STRIP_COMMENTS, '');

test/autoInject.js
@@ -224,4 +224,33 @@ describe('autoInject', () => {
             done()
         })
     })
+
+    it('should not be subject to ReDoS', () => {
+        // This test will timeout if the bug is present.
+        var someComments = 'text/*'.repeat(1000000)
+        expect(() => async.autoInject({
+            someComments,
+            a () {}
+        })).to.throw()
+    });
+
+    it('should properly strip comments in argument definitions', (done) => {
+        async.autoInject({
+            task1: function(task2, /* ) */ callback) {
+                callback(null, true);
+            },
+            task2: function task2(task3 // )
+                ,callback) {
+                callback(null, true);
+            },
+            task3: function task3(callback) {
+                callback(null, true);
+            }
+        },
+        (err, result) => {
+            expect(err).to.eql(null);
+            expect(result).to.deep.eql({task1: true, task2: true, task3: true});
+            done();
+        });
+    });
 });

commit cdaeeb840b5974f6096aeb0d5e21e0addfdf753d

fix: address edge case in autoInject comments
Unforunately, cdfb4917e6028c8f966276d6e792018c7fd2ae3c introduced issues
with certain edge cases for comments. Fixing the edge case in the
regular expression is likely to reintroduce the issue that commit was
addressing. This change replaces the regular expression entirely with
a function that uses `String.prototype` methods instead of regular
expressions. While more wordy, it is likely easier to read/understand
(and therefore easier to maintain) and it seems to be performant.

lib/autoInject.js
@@ -6,10 +6,42 @@ var FN_ARGS = /^(?:async\s+)?(?:function)?\s*\w*\s*\(\s*([^)]+)\s*\)(?:\s*{)/;
 var ARROW_FN_ARGS = /^(?:async\s+)?\(?\s*([^)=]+)\s*\)?(?:\s*=>)/;
 var FN_ARG_SPLIT = /,/;
 var FN_ARG = /(=.+)?(\s*)$/;
-var STRIP_COMMENTS = /(\/\*(?:[^/]|\/(?!\*))*\*\/)|\/\/.*$/mg;
+
+function removeDoubleSlash(string) {
+    return string.replace(/\/\/.*$/m, '');
+}
+
+function removeSlashStar(string, startIndex) {
+    let endIndex = string.indexOf('*/', startIndex);
+    endIndex = endIndex === -1 ? string.length - 1 : endIndex + 2;
+    return string.slice(0, startIndex) + string.slice(endIndex)
+}
+function stripComments(string) {
+    const doubleSlash = string.indexOf('//');
+    const slashStar = string.indexOf('/*');
+    if ((doubleSlash === -1) && (slashStar === -1)) {
+        // No comments found.
+        return string;
+    }
+    if ((doubleSlash !== -1) && (slashStar !== -1)) {
+        // Both kinds of comments found. Do the earliest one first so that we handle nested comments correctly.
+        if (doubleSlash < slashStar) {
+            return stripComments(removeDoubleSlash(string));
+        } else {
+            return stripComments(removeSlashStar(string, slashStar));
+        }
+    } else {
+        // Only one kind of comment found.
+        if (doubleSlash !== -1) {
+            return stripComments(removeDoubleSlash(string));
+        } else {
+            return stripComments(removeSlashStar(string, slashStar));
+        }
+    }
+}
 
 function parseParams(func) {
-    const src = func.toString().replace(STRIP_COMMENTS, '');
+    const src = stripComments(func.toString());
     let match = src.match(FN_ARGS)
     if (!match) {
         match = src.match(ARROW_FN_ARGS);

test/autoInject.js
@@ -243,13 +243,17 @@ describe('autoInject', () => {
                 ,callback) {
                 callback(null, true);
             },
-            task3: function task3(callback) {
+            task3: function task3(task4 /* /* )
+                */, callback) {
                 callback(null, true);
-            }
+            },
+            task4: function task4(callback) {
+                callback(null, true);
+            },
         },
         (err, result) => {
             expect(err).to.eql(null);
-            expect(result).to.deep.eql({task1: true, task2: true, task3: true});
+            expect(result).to.deep.eql({task1: true, task2: true, task3: true, task4: true});
             done();
         });
     });